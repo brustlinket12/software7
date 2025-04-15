e6ead20de325f5602cf76458b7f7e9436b3c2863
JGonzalez118 <josue08gl@gmail.com>
2025-04-14 23:04:24 -0500

    Update urls.py
    path a pagina de registro

--- blogproject/blogproject/blogapp/urls.py
+++ blogproject/blogproject/blogapp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import BlogListView, BlogDetailView, ReviewCreateView, CommentCreateView, BlogCreateView
+from .views import register
 
 app_name = 'blogapp'
 
@@ -10,4 +11,6 @@ urlpatterns = [
     path('blog/<int:pk>/', BlogDetailView.as_view(), name='blog_detail'),
     path('blog/<int:pk>/review/', ReviewCreateView.as_view(), name='add_review'),
     path('blog/<int:blog_pk>/review/<int:review_pk>/comment/', CommentCreateView.as_view(), name='add_comment'),
+    path('register/', register, name='create_user'),
+    # path('login/', LoginUser.as_view(), name='login_user'), queda hacerlo
 ]
