c2a6986a85e11741f69ee6cbb5dea5b4ea951a4a
JGonzalez118 <josue08gl@gmail.com>
2025-04-14 23:03:02 -0500

    Update views.py
    Añadido vista creación de usuario y LoginRequiredMixin en la creación de comentarios y reseñas.

--- blogproject/blogproject/blogapp/views.py
+++ blogproject/blogproject/blogapp/views.py
@@ -1,7 +1,11 @@
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, FormView
 from django.urls import reverse_lazy
 from .models import Blog, Review, Comment
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+from django.shortcuts import render, redirect
 
 class BlogListView(ListView):
     model = Blog
@@ -27,7 +31,7 @@ class BlogCreateView(LoginRequiredMixin, CreateView):
 
 
 
-class ReviewCreateView(CreateView):
+class ReviewCreateView(LoginRequiredMixin, CreateView): #Pide usuario logeado para crear review
     model = Review
     fields = ['rating', 'comment']
     template_name = 'blogapp/review_form.html'
@@ -41,7 +45,7 @@ class ReviewCreateView(CreateView):
         return reverse_lazy('blogapp:blog_detail', kwargs={'pk': self.kwargs['pk']})
 
 
-class CommentCreateView(CreateView):
+class CommentCreateView(LoginRequiredMixin, CreateView): #Pide usuario logeado para crear review
     model = Comment
     fields = ['content']
     template_name = 'blogapp/comment_form.html'
@@ -53,3 +57,24 @@ class CommentCreateView(CreateView):
 
     def get_success_url(self):
         return reverse_lazy('blogapp:blog_detail', kwargs={'pk': self.kwargs['blog_pk']})
+
+#TODO: redireccionar el usuario a la pagina de registro o la de inicio de sesión
+#TODO: si este intenta crear un blog, review o comentario sin haber iniciado sesión (por eso el LoginRequiredMixin de arriba)
+
+
+# crear usuarios
+class RegisterUser(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterUser(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('blogapp/base.html')
+    else:
+        form = RegisterUser()
+
+    return render(request, 'blogapp/create_user.html', {'form': form})
