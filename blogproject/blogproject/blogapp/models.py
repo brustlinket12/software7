3f020cd8d61424177222389ecb6edaf3001a02d6
JGonzalez118 <josue08gl@gmail.com>
2025-04-30 22:59:03 -0500

    Borrar blog fixed

--- blogproject/blogproject/blogapp/models.py
+++ blogproject/blogproject/blogapp/models.py
@@ -5,11 +5,24 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 # MODELOS
 
+class BlogManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(is_deleted=False)
+
 class Blog(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField(max_length=2000)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+    is_deleted = models.BooleanField(default=False)
+
+    objects = BlogManager()
+    all_objects = models.Manager()
+
+    def delete(self, *args  , **kwargs):
+        self.is_deleted = True
+        self.save()
+
 
     def __str__(self):
         return self.title
