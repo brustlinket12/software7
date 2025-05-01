3c9dc24bf923c75bc25a68a60718f6ecb8b19791
JGonzalez118 <josue08gl@gmail.com>
2025-05-01 02:10:32 -0500

    Update Profile & Admin
    Arreglado el boton de eliminar el blog en el perfil de usuario.
    
    El admin ahora puede ver los blogs que hayan sido marcados como borraos en /admin

--- blogproject/blogproject/blogapp/admin.py
+++ blogproject/blogproject/blogapp/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import Blog, Review, Comment
 
-admin.site.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    def get_queryset(self, request):
+        return Blog.all_objects.all()
+
+
+admin.site.register(Blog, BlogAdmin)
 admin.site.register(Review)
 admin.site.register(Comment)
